0d5a97f5d744fe4bda66cfa041f7e92278c12e32
= <gabrqueiroz97@gmail.com>
2020-04-07 12:34:52 -0300

    bootstrap feito

--- index.js
+++ index.js
@@ -1,16 +1,42 @@
-var express = require('express'); //Importa a lib express
-var app = express();//Inicia a lib
+
+const express = require('express'); //Importa a lib express
+const app = express();//Inicia a lib
+const bodyParser= require('body-parser')
 const knex = require('knex')
 const path = require('path')
 
-app.get('/all', function(req, res) {
-  res.send('Olá mundo');
-});
-app.get('/', function (req, res) {
+const db = knex({
+  client: 'mysql',
+  connection: { 
+  host:"127.0.0.1",
+  user: "root",
+  password: "",
+  database: "db"},
+  
+})
+
+
+app.get('/', function (req, res, next) {
   res.sendFile(__dirname + "/dist/index.html")
-});
+  });
 
 app.listen(8081,() => {
     console.log(' Servidor Ok ');
   });
   
+/*
+  //var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'dbuser',
+  password : 's3kreee7'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();*/
