4c3b6c13d11876ce7ef8bd203fb538ca524b9efb
= <gabrqueiroz97@gmail.com>
2020-04-10 10:12:36 -0300

    bd ok e id ok

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 
-const express = require('express'); //Importa a lib express
-const app = express();//Inicia a lib
-const bodyParser= require('body-parser')
 const knex = require('knex')
 const path = require('path')
 const restify = require('restify');
 const errs = require('restify-errors');
-
-
 
 const db = knex({
   client: 'mysql',
@@ -24,22 +19,108 @@ const server = restify.createServer({
   version: '1.0.0'
 });
 
+
 server.get('/', restify.plugins.serveStatic({
   directory: './dist',
   file: 'index.html'
 
 }));
 
+
+server.use(restify.plugins.acceptParser(server.acceptable));
+server.use(restify.plugins.queryParser());
+server.use(restify.plugins.bodyParser());
+
+
 server.get('/read', function (req, res, next) {
 
-  db('rest').then((dados)=>{
+  db('task').then((dados)=>{
     res.send(dados)
   },next)
 
   return next();
 });
 
+
+
+server.post('/save',(req, res)=>{
+  db('task')
+  .insert(req.body)
+  .then (dados => {
+    return res.send(dados)
+  })
+    
+  })
+  
+
+  server.post('/create', function (req, res, next) {
+
+    db('task')
+      .insert(req.body)
+      .then((dados)=>{
+      res.send(dados)
+    },next)
+  
+    return next();
+  });
+  
+
+  server.get('/show/:id', function (req, res, next) {
+    const {id} = req.params
+  
+    db('task')
+      .where('id', id)
+      .first()
+      .then((dados)=>{
+        if(!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+  
+      res.send(dados)
+    },next)
+  
+    return next();
+  });
+
+
+  server.put('/update/:id', function (req, res, next) {
+    const {id} = req.params
+  
+    db('task')
+      .where('id', id)
+      .update(req.body)
+      .then((dados)=>{
+        if(!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+        
+      res.send('dados atualizados!')
+    },next)
+  
+    return next();
+  });
+
+  
+    server.del('/delete/:id', function (req, res, next) {
+      const {id} = req.params
+    
+      db('task')
+        .where('id', id)
+        .delete()
+        .then((dados)=>{
+          if(!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
+          
+        res.send('dados excluidosç!')
+      },next)
+    
+      return next();
+    });
+
+
+    
+  server.listen(8081,() => {
+    console.log(' Servidor Ok ');
+  });
+
 ///////////
+
+/*
 app.set('view engine', 'html')
 exports.index = function(req, res){
   res.render('index');
@@ -59,114 +140,7 @@ app.get('/all',(req, res)=>{
    
  })
   })
-
-app.post('/save',(req, res)=>{
-db('task')
-.insert(req.body)
-.then (dados => {
-  return res.send(dados)
-})
-  
-})
-
-app.listen(8081,() => {
-    console.log(' Servidor Ok ');
-  });
-  
-/*
-const restify = require('restify');
-const errs = require('restify-errors');
-
-
-const server = restify.createServer({
-  name: 'myapp',
-  version: '1.0.0'
-});
-
-server.use(restify.plugins.acceptParser(server.acceptable));
-server.use(restify.plugins.queryParser());
-server.use(restify.plugins.bodyParser());
-
-
-
-server.listen(8080, function () {
-  console.log('%s listening at %s', server.name, server.url);
-});
-
-var knex = require('knex')({
-  client: 'mysql',
-  connection: {
-    host : '127.0.0.1',
-    user : 'root',
-    password : '',
-    database : 'db'
-    }
-});
-
-server.get('/', restify.plugins.serveStatic({
-  directory: './dist',
-  file: 'index.html'
-
-}));
-/////////////////////////////
-server.get('/read', function (req, res, next) {
-
-  knex('rest').then((dados)=>{
-    res.send(dados)
-  },next)
-
-  return next();
-});
-
-server.post('/create', function (req, res, next) {
-
-  knex('rest')
-    .insert(req.body)
-    .then((dados)=>{
-    res.send(dados)
-  },next)
-
-  return next();
-});
-
-server.get('/show/:id', function (req, res, next) {
-  const{id} = req.params
-
-  knex('rest')
-    .where('id', id)
-    .first()
-    .then((dados)=>{
-      if(!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
-    res.send(dados)
-  }, next)
-  return next();
-});
-
-server.put('/update/:id', function (req, res, next) {
-const{id} = req.params
-
-  knex('rest')
-    .update(req.body)
-    .where('id',id)
-    .then((dados)=>{
-      if(!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
-    res.send("atualizado com sucesso")
-  },next)
-
-  return next();
-});
-
-server.del('/delete/:id', function (req, res, next) {
-  const{id} = req.params
-  
-    knex('rest')
-      .delete()
-      .where('id',id)
-      .then((dados)=>{
-        if(!dados) return res.send(new errs.BadRequestError('nada foi encontrado'))
-      res.send("deletado com sucesso")
-    },next)
-  
-    return next();
-  });
 */
+ 
+
+
